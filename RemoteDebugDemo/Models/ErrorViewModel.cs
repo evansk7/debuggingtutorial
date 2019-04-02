96ec055e11481ed0ee29e54afbde4ea13c202514
Kevin Evans <kevin.evans@activeops.com>
2019-04-02 15:33:53 -0400

    Add project files.

---
+++ RemoteDebugDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RemoteDebugDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
