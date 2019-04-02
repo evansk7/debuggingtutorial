0d22e1cfb4b052a155b5d8547a6a8f4c358ccc67
Kevin Evans <underwatergeek@hotmail.com>
2019-04-02 16:40:55 -0400

    initial commit

---
+++ RemoteDebugDemo/Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RemoteDebugDemo.Models;
+
+namespace RemoteDebugDemo.Controllers
+{
+    [Authorize]
+    public class BlogController : Controller
+    {
+        [HttpPost]
+        public IActionResult Add(Blog blog)
+        {
+
+            using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+            }
+
+            return new RedirectToActionResult("Index", "Home", null);
+        }
+
+        public IActionResult Test()
+        {
+            return View("Home/Index");
+        }
+    }
+}
--- RemoteDebugDemo/Controllers/HomeController.cs
+++ RemoteDebugDemo/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace RemoteDebugDemo.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+                return View();
         }
 
         public IActionResult About()
